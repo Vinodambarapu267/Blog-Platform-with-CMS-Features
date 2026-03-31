cb868f62873c0a926c66ef327acb0e2c65ed8933
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-03-31 23:17:35 +0530

    initial: first commit

---
+++ Categories-&-Tag-service/src/main/java/com/example/demo/CategoriesService1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoriesService1Application.class, args);
+	}
+
+}
