149162a850afe5ddebd0fd1e009371d6b66dea3a
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-07-01 23:04:12 +0530

    fix(auth-service): update the permit for actutors

--- Auth-service/src/main/java/com/example/demo/config/AuthConfig.java
+++ Auth-service/src/main/java/com/example/demo/config/AuthConfig.java
@@ -34,6 +34,7 @@ public class AuthConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+            		.requestMatchers("//actutors/**").permitAll()
             		.requestMatchers("/api/v1/auth/register",
             		        "/api/v1/auth/login",
             		        "/api/v1/auth/token",
