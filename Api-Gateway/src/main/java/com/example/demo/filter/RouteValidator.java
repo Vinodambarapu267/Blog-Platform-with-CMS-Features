ea17f178dbaa3632f20e2a2a6dbb4f23718ddcf4
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-07-10 16:02:54 +0530

    feat: add API gateway routing and security

--- Api-Gateway/src/main/java/com/example/demo/filter/RouteValidator.java
+++ Api-Gateway/src/main/java/com/example/demo/filter/RouteValidator.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
-
 @Component
 public class RouteValidator {
 
@@ -23,7 +22,8 @@ public class RouteValidator {
             "/api/v1/categories/**",
             "/api/v1/tags/**",
             "/api/v1/users/**",
-            "/api/v1/comments/posts/*/comments"
+            "/api/v1/comments/posts/*/comments",
+            "/api/v1/posts/findpostsbyuserid/*"
     );
 
     private static final List<String> OPEN_POST_ENDPOINTS = List.of(
@@ -31,18 +31,16 @@ public class RouteValidator {
             "/api/v1/users/createuser"
     );
 
-    public Predicate<ServerHttpRequest> isSecured = request -> {
-        String path = request.getURI().getPath();
-        HttpMethod method = request.getMethod();
+	public Predicate<ServerHttpRequest> isSecured = request -> {
+		String path = request.getURI().getPath();
+		HttpMethod method = request.getMethod();
 
-        boolean isOpenGet = method == HttpMethod.GET &&
-                OPEN_GET_ENDPOINTS.stream()
-                        .anyMatch(pattern -> pathMatcher.match(pattern, path));
+		boolean isOpenGet = HttpMethod.GET.equals(method)
+				&& OPEN_GET_ENDPOINTS.stream().anyMatch(pattern -> pathMatcher.match(pattern, path));
 
-        boolean isOpenPost = method == HttpMethod.POST &&
-                OPEN_POST_ENDPOINTS.stream()
-                        .anyMatch(pattern -> pathMatcher.match(pattern, path));
-
-        return !(isOpenGet || isOpenPost);
-    };
+		boolean isOpenPost = HttpMethod.POST.equals(method)
+				&& OPEN_POST_ENDPOINTS.stream().anyMatch(pattern -> pathMatcher.match(pattern, path));
+		System.out.println("path : " + path);
+		return !(isOpenGet || isOpenPost);
+	};
 }
