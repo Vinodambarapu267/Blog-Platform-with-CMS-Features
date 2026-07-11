b5990cadc6caf03c1f4e54dd9f87aedef8a25ff2
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-07-11 15:10:03 +0530

    remove the @preAuthorize for the findbyId method it is block the notification-service endpoints

--- User-Service/src/main/java/com/example/demo/controller/UserController.java
+++ User-Service/src/main/java/com/example/demo/controller/UserController.java
@@ -67,7 +67,6 @@ public class UserController {
 
 	@GetMapping("/{userId}")
 	@RateLimiter(name = "myRateLimiter")
-	@PreAuthorize("hasAuthority('USER_READ')")
 	public UserDto findByUserId(@PathVariable Long userId) {
 		return userService.findById(userId);
 
